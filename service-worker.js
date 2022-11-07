5318ba172fff9327a5a85ee293a0f012372a0405
tedinasuit <larshoeijmans@gmail.com>
2022-11-07 09:16:14 +0100

    Revert "Second iteration"
    This reverts commit 8c963e06f06fde472fb0cfc250d8b64aa6d57ac8.

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,16 @@
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
-);
-
-workbox.routing.registerRoute(
-  ({request}) => request.destination === 'image',
-  new workbox.strategies.NetworkFirst()
-);
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('static-v1')
-    .then(cache => cache.addAll([
-      '/offline.html'
-    ]))
-  )
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-    .then(response => response || fetch(event.request))
-    .catch(() => {
-      if (event.request.mode == 'navigate') {
-        return caches.match('/offline.html');
-      }
-    })
-  );
-});
+self.addEventListener("install",(installing)=>{
+    console.log("Service Worker: I am being installed, hello world!");
+  });
+  
+  self.addEventListener("activate",(activating)=>{	
+    console.log("Service Worker: All systems online, ready to go!");
+  });
+  
+  self.addEventListener("fetch",(fetching)=>{   
+    console.log("Service Worker: User threw a ball, I need to fetch it!");
+  });
+  
+  self.addEventListener("push",(pushing)=>{
+      console.log("Service Worker: I received some push data, but because I am still very simple I don't know what to do with it :(");
+  })
+  
