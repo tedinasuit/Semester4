bbac89396490d81fd754f39f8ef3ef05eba8f5a8
tedinasuit <larshoeijmans@gmail.com>
2022-11-07 09:23:24 +0100

    Fourth Iteration
    The favourites function now works, although limited. It seems to work best for Chrome browsers.
    
    If you press the star button on either Brazil or Greece, it will be added to favourites.html. 'Delete all' will clear LocalStorage. Due to time constraints, we haven't been able to get all buttons work in a similar way, but that should be relatively easy to do.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const image_input = document.querySelector("#image_input");
 var uploaded_image = "";
+const ul = document.querySelector(".myclass");
+const input = document.getElementById('item');
+const greece = document.getElementById('Greece');
+const brazil = document.getElementById('Brazil');
+let itemsArray = localStorage.getItem('items') ?
+JSON.parse(localStorage.getItem('items')) : [];
+
 
 image_input.addEventListener("change", function(){
     const reader = new FileReader();
@@ -16,3 +23,29 @@ const lists = document.querySelectorAll('.hs');lists.forEach(el => {
   const n = el.children.length;
   el.style.setProperty('--total', n);
 });
+
+itemsArray.forEach(divMakerB);
+function divMakerB (text){
+    const div = document.createElement('div');
+    div.innerHTML = text;
+    ul.appendChild(div);
+    div.id = "Brazil";
+}
+
+function addB(){
+    itemsArray.push(brazil.innerHTML);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    divMakerB(brazil.innerHTML);
+}
+
+function addG(){
+    itemsArray.push(greece.innerHTML);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
+    divMakerG(greece.innerHTML);
+}
+
+function delB(){
+    localStorage.clear();
+    ul.innerHTML = '';
+    itemsArray = [];
+}
